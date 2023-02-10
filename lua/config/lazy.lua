05d6c4ad2a6b41bfdee55c0f2014d9406c860bf5
wirr <wirr@banana.local>
2023-02-10 15:00:42 +0100

    Disable plugin import

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "wirr00/LazyVim", import = "lazyvim.plugins" },
+    -- { "wirr00/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
